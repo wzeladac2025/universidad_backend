dd132a6ef0b6fc37ecde0b305564e643b3339276
karentejaxun <karentejaxun@gmail.com>
2025-09-16 19:21:40 -0600

    server modi

--- server.js
+++ server.js
@@ -1,7 +1,74 @@
 // Importamos el modulo express 
+require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve Swagger documentation
+const swaggerSpec = require("./app/config/swagger.config");
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Test
+ *     tags: [Servicios Up]
+ *     description: Test Proyecto Levantado
+ *     responses:
+ *       200:
+ *         description: Test Proyecto Levantado
+ */
+app.get("/", (req, res) => {
+  res.json({ message: "UMG Web Universidad" });
+});
+
+try {
+  require("./app/routes/estudiante.routes.js")(app);
+  console.log("✅ estudiante.routes.js cargado correctamente");
+} catch (err) {
+  console.error("❌ Error al cargar estudiante.routes.js:", err.message);
+}
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor iniciado correctamente en el puerto ${PORT}.`);
+});
+
+try {
+  require("./app/routes/notificacion_pago.routes.js")(app);
+  console.log("notificacion_pago.routes.js cargado correctamente");
+} catch (err) {
+  console.error("Error al cargar notificacion_pago.routes.js:", err.message);
+}
+
+try {
+  require("./app/routes/tipo_notificacion.routes.js")(app);
+  console.log("tipo_notificacion.routes.js cargado correctamente");
+} catch (err) {
+  console.error("Error al cargar tipo_notificacion.routes.js:", err.message);
+}
+
+/*const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+
 
 const app = express();
 
@@ -28,4 +95,4 @@ require("./app/routes/tipo_notificacion.route")(app);
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Servidor levantado en puerto ${PORT}.`);
-});
+});*/
