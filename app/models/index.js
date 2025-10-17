b00066a36050e216e5e82dc540421d289b16ff42
gfdiego <126045984+gfdiego@users.noreply.github.com>
2025-10-16 19:11:33 -0600

    Actualización en index.js del modelo de datos

--- app/models/index.js
+++ app/models/index.js
@@ -28,18 +28,20 @@ db.estudiante = require("./estudiante.model.js")(sequelize, Sequelize);
 db.docente = require("./docente.model.js")(sequelize, Sequelize);
 db.carrera = require("./carrera.model.js")(sequelize, Sequelize);
 db.estudianteCarrera = require("./estudianteCarrera.model.js")(sequelize, Sequelize);
-// Relaciones muchos a muchos
+// Relación: un estudiante puede estar en muchas carreras
 db.estudiante.belongsToMany(db.carrera, {
-  through: db.estudianteCarrera,
-  foreignKey: 'estudianteId',
-  otherKey: 'carreraId'
+  through: db.estudianteCarrera,     // Modelo intermedio que representa la relación
+  foreignKey: 'estudianteId',        // Clave que conecta al estudiante
+  otherKey: 'carreraId'              // Clave que conecta a la carrera
 });
 
+// Relación inversa: una carrera puede tener muchos estudiantes
 db.carrera.belongsToMany(db.estudiante, {
-  through: db.estudianteCarrera,
-  foreignKey: 'carreraId',
-  otherKey: 'estudianteId'
+  through: db.estudianteCarrera,     // Usamos el mismo modelo intermedio
+  foreignKey: 'carreraId',           // Clave que conecta a la carrera
+  otherKey: 'estudianteId'           // Clave que conecta al estudiante
 });
+
 
 // Relación carrera → docente (coordinador)
 db.carrera.belongsTo(db.docente, {
